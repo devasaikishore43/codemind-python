71eceecdf6807388e5da961bfbc7ad0bf8e0ac00
T.Deva Sai Kishore <81376904+devasaikishore43@users.noreply.github.com>
2022-05-01 18:36:54 +0530

    n=input() n=n.lower() n=n.split(' ') z=['a','e','i','o','u'] c=0 for d in n:     for i in range(len(d)//2):         if d[i] in z and d[len(d)-(i+1)] not in z:             c+=1         if d[i] not in z and d[len(d)-(i+1)] in z:             c+=1 print(c)

---
+++ count_vowel_consonent_pairs_in_words.py
@@ -0,0 +1,14 @@
+n=input()
+n=n.lower()
+n=n.split(' ')
+z=['a','e','i','o','u']
+c=0
+for d in n:
+    for i in range(len(d)//2):
+        if d[i] in z and d[len(d)-(i+1)] not in z:
+            c+=1
+        if d[i] not in z and d[len(d)-(i+1)] in z:
+            c+=1
+print(c)
+
+        
