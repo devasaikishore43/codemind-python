cb38847a990f11de2f952e9e04fe6ea0f9df17e0
T.Deva Sai Kishore <81376904+devasaikishore43@users.noreply.github.com>
2022-09-25 19:17:44 +0530

    a=int(input()) l=list(map(int,input().split())) l.sort(reverse=True) b,c=0,0 for i in l:     if c<b:         c+=i     else:         b+=i if c==b:     print("YES") else:     print("NO")

--- Petrol_Lock.py
+++ Petrol_Lock.py
@@ -1,15 +1,13 @@
 a=int(input())
-nums=list(map(int,input().split()))
-answer=[[]]
-b = sum(nums)//2
-c = False
-for n in nums:
-    answer+=[i+[n] for i in answer]
-for i in answer:
-    if sum(i)==b:
-        c = True
-        break
-if c:
+l=list(map(int,input().split()))
+l.sort(reverse=True)
+b,c=0,0
+for i in l:
+    if c<b:
+        c+=i
+    else:
+        b+=i
+if c==b:
     print("YES")
 else:
     print("NO")
